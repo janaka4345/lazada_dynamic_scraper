904fefe94d2d72c2dcee49a8c2ba008ab58f4bc6
Janaka dev <janakchamantha12@gmail.com>
2024-08-23 23:30:24 +0530

    other:spider data extraction try

--- lazadascrape/lazadascrape/spiders/lazadaspider.py
+++ lazadascrape/lazadascrape/spiders/lazadaspider.py
@@ -1,10 +1,30 @@
 import scrapy
+from scrapy_playwright.page import PageMethod
 
 
 class LazadaspiderSpider(scrapy.Spider):
     name = "lazadaspider"
-    allowed_domains = ["example.com"]
-    start_urls = ["https://example.com"]
+    allowed_domains = ["www.lazada.com.my", "scrapeops.io"]
+
+    def start_requests(self):
+        url = "https://www.lazada.com.my/tag/keychain/"
+        yield scrapy.Request(
+            url,
+            meta=dict(
+                playwright=True,
+                playwright_include_page=True,
+                PageMethods=[PageMethod("wait_for_selector", "div.ant-cole")],
+                errback=self.error,
+            ),
+            callback=self.parse,
+        )
 
     def parse(self, response):
-        pass
+        items = response.xpath('//div[contains(@class,"Bm3ON")]')
+        print("***************************")
+        print(items)
+        print("***************************")
+
+    async def error(self, faliure):
+        page = faliure.request.meta["playwright"]
+        await page.close
